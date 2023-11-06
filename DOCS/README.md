48162222ea86993c55e363a0c840eb01ee95c444
javac0f <jcof28@gmail.com>
2023-11-06 15:38:41 -0500

    Uploading existing work to Github

---
+++ DOCS/README.md
@@ -0,0 +1,22 @@
+# MICROPLASTIC SEGMENTATION
+
+<h1>
+Author: Jared Coffey<br>    
+Date: 3/19/2022<br>
+Parverio Inc. Spring 2022 Internship
+</h1>
+
+
+
+
+x_train = our "Raw" images
+y_train = our "Ground Truth" image masks
+x_test = our Raw test images
+y_test = our Model's predictions
+
+
+
+
+
+---
+<h1>SOURCES:</h1>
---
+++ config.py
@@ -0,0 +1,11 @@
+X_TRAIN_DATA:str = "C:/Users/jcof2/Documents/Coding_Projects/Microplastic_Segmentation/DATA/train_data/Raw_tiffs"
+Y_TRAIN_DATA:str = "C:/Users/jcof2/Documents/Coding_Projects/Microplastic_Segmentation/DATA/train_data/Ground_Truth"
+
+
+X_PRED_DATA:str = "C:/Users/jcof2/Documents/Coding_Projects/Microplastic_Segmentation/DATA/test_data/Raw_tiffs"
+Y_PRED_DATA:str = ""
+
+IMAGE_SIZE:int = 256
+BATCH_SIZE:int = 25
+
+TEST_SPLIT:int = 0.2
